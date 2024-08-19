9e63b6c80fdb906d92e0f2d75c7ce0ef9e65d38b
JanefrancessC <janefrancesschioma@gmail.com>
2024-08-19 17:12:12 +0100

    strncmp

---
+++ ex00/ft_strcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cokeke <cokeke@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 16:48:23 by cokeke            #+#    #+#             */
+/*   Updated: 2024/08/19 17:11:10 by cokeke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	checker;
+
+	checker = ft_strcmp("hello", "hello");
+	printf("%d", checker);
+	return (0);
+}
